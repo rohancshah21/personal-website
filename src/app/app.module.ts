8cb59a48ea0f99d546e412b25c5cc92971148dee
Rohan C. Shah <rohancshah@Rohans-MacBook-Pro.local>
2022-07-17 21:40:14 -0400

    initial components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
